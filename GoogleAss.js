f55398cf3b2a178545e035ab2f347eb8c7616c85
genisx3 <105855743+genisx3@users.noreply.github.com>
2025-01-16 15:31:54 +0100

    Create GoogleAss.js

---
+++ GoogleAss.js
@@ -0,0 +1,11 @@
+{
+  module: "MMM-GoogleAssistant",
+  configDeepMerge: true,
+  config: {
+    assistantConfig: {
+      lang: "en-US",
+      latitude: 47.0733,
+      longitude: 15.4169,
+    }
+  }
+},
